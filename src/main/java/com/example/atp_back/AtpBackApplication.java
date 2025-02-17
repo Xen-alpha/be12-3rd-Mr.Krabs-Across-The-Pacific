c6978b532fc786f7ef5d35a74c04dda27aaf6bb9
celarim <k6jun@kookmin.ac.kr>
2025-02-17 16:37:25 +0900

    [Init] Project Initialized

---
+++ src/main/java/com/example/atp_back/AtpBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.atp_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtpBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtpBackApplication.class, args);
+    }
+
+}
