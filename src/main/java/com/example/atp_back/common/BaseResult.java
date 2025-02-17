f1499558fed27d42d29582d1205191eb38f9fc6d
Xen-alpha <hky7548@naver.com>
2025-02-17 18:56:09 +0900

    [Feat] add error response declaration

---
+++ src/main/java/com/example/atp_back/common/BaseResult.java
@@ -0,0 +1,4 @@
+package com.example.atp_back.common;
+
+public interface BaseResult {
+}
---
+++ src/main/java/com/example/atp_back/common/ErrorResult.java
@@ -0,0 +1,14 @@
+package com.example.atp_back.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ErrorResult implements BaseResult {
+    
+}
