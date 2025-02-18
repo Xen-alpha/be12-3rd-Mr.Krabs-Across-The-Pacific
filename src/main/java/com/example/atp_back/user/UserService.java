290889180d211183da9406b51b8ba42b234cc949
Xen-alpha <hky7548@naver.com>
2025-02-18 19:25:51 +0900

    [Feat] implement signup service
    서비스 계층에 DB 가입 정보를 기입하는 코드를 넣었습니다
    아직 완벽하게 구현된 것은 아닙니다
    
    Related to: #7

--- src/main/java/com/example/atp_back/user/UserService.java
+++ src/main/java/com/example/atp_back/user/UserService.java
@@ -1,16 +1,36 @@
 package com.example.atp_back.user;
 
+import com.example.atp_back.user.model.SignupReq;
+import com.example.atp_back.user.model.SignupResp;
 import com.example.atp_back.user.model.User;
+import com.example.atp_back.user.model.UserTier;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
+@RequiredArgsConstructor
 @Service
 public class UserService implements UserDetailsService {
     private UserRepository userRepository;
+
+    public void RegisterUser(SignupReq signupReq, MultipartFile file) {
+        LocalDate thistime = LocalDate.now();
+        User user = User.builder().name(signupReq.getName()).email(signupReq.getEmail())
+                .password(signupReq.getPassword())
+                .profileImage(signupReq.getImage())
+                .createdAt(thistime)
+                .updatedAt(thistime)
+                .tierGrade(UserTier.builder().grade("Bronze").build())
+                .build();
+        userRepository.save(user);
+        // TODO: File upload
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
--- src/main/java/com/example/atp_back/user/model/SignupReq.java
+++ src/main/java/com/example/atp_back/user/model/SignupReq.java
@@ -1,5 +1,12 @@
 package com.example.atp_back.user.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class SignupReq {
     private String name;
     private String email;
