220f7f6b15768831285eedd2f5a25edc0549b834
bdt6246 <a01094079599@gmail.com>
2025-02-18 23:14:12 +0900

    [Feat]포트폴리오 기능을 위한 Respository, Service 추가 및 Controller 코드 작성 포트폴리오 등록, 조회, 목록 조회 기능

--- src/main/java/com/example/atp_back/portfolio/PortfolioController.java
+++ src/main/java/com/example/atp_back/portfolio/PortfolioController.java
@@ -1,4 +1,36 @@
 package com.example.atp_back.portfolio;
 
+import com.example.atp_back.portfolio.model.request.PortfolioCreateReqDto;
+import com.example.atp_back.portfolio.model.response.PortfolioInstanceRespDto;
+import com.example.atp_back.portfolio.model.response.PortfolioPageRespDto;
+import com.example.atp_back.user.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/portfolio")
 public class PortfolioController {
+    private final PortfolioService portfolioService;
+
+    @PostMapping("/register")
+    public void register(@AuthenticationPrincipal User user, @RequestBody PortfolioCreateReqDto dto) {
+        portfolioService.register(user, dto);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<PortfolioPageRespDto> list(int page, int size) {
+        PortfolioPageRespDto response = portfolioService.list(page, size);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{portfolioIdx}")
+    public ResponseEntity<PortfolioInstanceRespDto> read(@PathVariable Long portfolioIdx) {
+        PortfolioInstanceRespDto response = portfolioService.read(portfolioIdx);
+
+        return ResponseEntity.ok(response);
+    }
 }
