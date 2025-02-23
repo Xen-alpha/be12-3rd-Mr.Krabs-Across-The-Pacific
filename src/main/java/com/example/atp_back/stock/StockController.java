6a8b6ac838f64c1823d4b84c40e61eadeab8eb9f
celarim <k6jun@kookmin.ac.kr>
2025-02-23 19:18:35 +0900

    [Docs] Stock Swagger 작성
    주식 기능들의 Swagger 작성 및
    댓글 입력 공백 불가 설정

--- src/main/java/com/example/atp_back/stock/StockController.java
+++ src/main/java/com/example/atp_back/stock/StockController.java
@@ -7,6 +7,8 @@ import com.example.atp_back.stock.service.StockReplyLikesService;
 import com.example.atp_back.stock.service.StockReplyService;
 import com.example.atp_back.stock.service.StockService;
 import com.example.atp_back.user.model.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -17,11 +19,18 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/stock")
+@Tag(name = "주식 관련 기능")
 public class StockController {
     private final StockService stockService;
     private final StockReplyService stockReplyService;
     private final StockReplyLikesService stockReplyLikesService;
 
+    @Operation(summary = "주식 상세페이지 조회", description = """
+            /stock/detail/{idx} 값을 입력 받는다. \n
+            idx 값을 가지는 주식의 id, 주식 이름, 주식 코드, 주식 거래소를 반환하며 \n
+            해당 주식의 댓글들을 반환 받는다. \n
+            주식의 댓글은, 댓글의 id, 주식의 id, 작성자의 id, 작성자의 이름, 댓글 내용, 댓글 작성 시간, 댓글 수정 시간, 좋아요 개수를 전달받는다.
+            """)
     @GetMapping("/detail/{idx}")
     public ResponseEntity<BaseResponse<StockDetailResp>> getStock(@PathVariable long idx) {
         BaseResponse<StockDetailResp> resp = new BaseResponse<>();
@@ -29,6 +38,11 @@ public class StockController {
         return ResponseEntity.ok(resp);
     }
 
+    // TODO : 주식 목록 페이지네이션, 주식 반환값 확정
+    @Operation(summary = "주식 목록 조회", description = """
+            /stock/list 값을 입력 받는다. \n
+            주식들의 id, 주식 이름, 주식 코드, 주식 거래소 값들을 반환한다.
+            """)
     @GetMapping("/list")
     public ResponseEntity<BaseResponse<List<StockDetailResp>>> getStocks() {
         BaseResponse<List<StockDetailResp>> resp = new BaseResponse<>();
@@ -37,7 +51,10 @@ public class StockController {
     }
 
 
-
+    @Operation(summary = "주식 댓글 작성", description = """
+            /stock/reply/{stockId} 값을 입력 받는다. 인가된 사용자만 사용할 수 있다. \n
+            contents 값을 전달 받아서 인가 사용자와 입력받은 주식 id 값을 바탕으로 댓글을 저장한다.
+            """)
     @PostMapping("/reply/{stockId}")
     public ResponseEntity<BaseResponse<String>> PostStockReply(@RequestBody StockReplyRegisterReq dto,
                                                               @AuthenticationPrincipal User user,
@@ -48,6 +65,10 @@ public class StockController {
         return ResponseEntity.ok(resp);
     }
 
+    @Operation(summary = "주식 댓글 좋아요 누르기", description = """
+            /stock/reply/likes/{replyId} 값을 전달받는다. 인가된 사용자만 사용할 수 있다. \n
+            인가 사용자와 입력받은 댓글 id 값을 바탕으로 좋아요를 저장한다.
+            """)
     @PostMapping("/reply/likes/{replyId}")
     public ResponseEntity<BaseResponse<String>> LikeReply(@AuthenticationPrincipal User user,
                                                            @PathVariable Long replyId) {
--- src/main/java/com/example/atp_back/stock/model/req/StockReplyRegisterReq.java
+++ src/main/java/com/example/atp_back/stock/model/req/StockReplyRegisterReq.java
@@ -3,10 +3,14 @@ package com.example.atp_back.stock.model.req;
 import com.example.atp_back.stock.model.Stock;
 import com.example.atp_back.stock.model.StockReply;
 import com.example.atp_back.user.model.User;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class StockReplyRegisterReq {
+    @Schema(example = "댓글 내용!!!")
+    @NotBlank
     private String contents;
 
     public StockReply toEntity(User user, Stock stock) {
