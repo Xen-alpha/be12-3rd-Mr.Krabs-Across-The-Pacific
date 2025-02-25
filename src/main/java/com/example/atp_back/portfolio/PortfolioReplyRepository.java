5a74e9519e10954507df7e679fa7a4ddbdc1ffe1
celarim <k6jun@kookmin.ac.kr>
2025-02-25 16:43:22 +0900

    [Feat] Portfolio 댓글 작성, 조회 기능 구현
    일단 페이지 처리는 안했습니다.
    1. 댓글 서비스, 레포지토리, 엔티티, dto작성 완료
    2. read기능 할때 기존의 instance가 아닌 detail이라는 기존 instacedto에 댓글을 추가한 dto로 반환
    3. 댓글 작성기능 /portfolio/reply/{idx}로 post요청 보내면 idx값 반환

---
+++ src/main/java/com/example/atp_back/portfolio/PortfolioReplyRepository.java
@@ -0,0 +1,7 @@
+package com.example.atp_back.portfolio;
+
+import com.example.atp_back.portfolio.model.entity.PortfolioReply;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PortfolioReplyRepository extends JpaRepository<PortfolioReply, Long> {
+}
