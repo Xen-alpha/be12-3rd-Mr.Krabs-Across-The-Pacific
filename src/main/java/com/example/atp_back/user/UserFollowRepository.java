411716377d9b0be33e42d28d0cab2ab7b5627705
Xen-alpha <hky7548@naver.com>
2025-03-18 17:38:32 +0900

    [Fix] add jpql to user and user follow tables
    사용자 쪽 N+1 문제를 JPQL 쿼리를 직접 작성하여 해결합니다

--- src/main/java/com/example/atp_back/user/UserFollowRepository.java
+++ src/main/java/com/example/atp_back/user/UserFollowRepository.java
@@ -3,12 +3,26 @@ package com.example.atp_back.user;
 import com.example.atp_back.user.model.User;
 import com.example.atp_back.user.model.follow.UserFollow;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface UserFollowRepository extends JpaRepository<UserFollow, Long> {
+    // TODO: N+1 문제 해결할 것 -> SELECT ... FROM user JOIN user_follow WHERE user_follow.follower = ... AND user_follow.followee
+    @Query("select f from user_follow f " +
+            "join fetch f.followee f1 " +
+            "join fetch f.follower f2 " +
+            "where f.followee = :followee")
     public List<UserFollow> findAllByFolloweeOrderByDate(User followee);
+    @Query("select f from user_follow f " +
+            "join fetch f.followee f1 " +
+            "join fetch f.follower f2 " +
+            "where f.follower = :follower")
     public List<UserFollow> findAllByFollowerOrderByDate(User follower);
+    @Query("select f from user_follow f " +
+            "join fetch f.followee f1 " +
+            "join fetch f.follower f2 " +
+            "where f.followee = :followee and f.follower = :follower")
     public Optional<UserFollow> findByFolloweeAndFollower(User followee, User follower);
 }
--- src/main/java/com/example/atp_back/user/UserRepository.java
+++ src/main/java/com/example/atp_back/user/UserRepository.java
@@ -2,8 +2,17 @@ package com.example.atp_back.user;
 
 import com.example.atp_back.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    // TODO: N+1 문제 해결할 것: SELECT ... FROM user JOIN  WHERE user.email = ...
+    @Query("select u from user u join fetch u.tier t" +
+            " join fetch u.portfolio p " +
+            "join fetch u.follower f " +
+            "join fetch u.followee g " +
+            "where u.email = :email")
     Optional<User> findByEmail(String email);
 }
