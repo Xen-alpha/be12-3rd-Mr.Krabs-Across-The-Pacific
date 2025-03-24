35d7a85eead964fc9f4a16a4fc76844afb35bbcd
Xen-alpha <hky7548@naver.com>
2025-03-24 12:17:22 +0900

    [Fix] fix jpql to work properly on user related situation

--- src/main/java/com/example/atp_back/user/UserRepository.java
+++ src/main/java/com/example/atp_back/user/UserRepository.java
@@ -10,7 +10,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     // TODO: N+1 문제 해결할 것: SELECT ... FROM user JOIN  WHERE user.email = ...
     @Query("select u from User u " +
-            "join fetch u.portfolios p " +
-            "where u.email = :email")
+            "where u.email = :email") // fetch 조인은 OneToMany 관계들에 대해 한 번만 쓸 수 있다.
     Optional<User> findByEmail(String email);
 }
